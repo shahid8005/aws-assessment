accd128a46e06864d7a83ad00d891fc55a7a650e
Shahid1044 <118257246+Shahid1044@users.noreply.github.com>
2026-02-28 13:49:15 +1030

    update principle

--- infra/modules/regional_stack/main.tf
+++ infra/modules/regional_stack/main.tf
@@ -185,7 +185,10 @@ data "archive_file" "dispatcher_zip" {
 data "aws_iam_policy_document" "lambda_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals { type="Service", identifiers=["lambda.amazonaws.com"] }
+    principals { 
+      type="Service" 
+      identifiers=["lambda.amazonaws.com"] 
+}
   }
 }
 
